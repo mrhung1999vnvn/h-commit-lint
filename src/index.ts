9fa6ced5dd759f2c2ef039d93262e4aa9ba3cf61
hung.pham5 <hung.pham5@mservice.com.vn>
2024-03-29 17:33:43 +0700

    init projecdt

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import ChalkLog from 'chalk';
+
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+    formatter: '@commitlint/format',
+    ignores: [commit => commit === ''],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: new RegExp(
+                /^(\w+)(?:\(([\w\s]+)\))?: (([A-Z]+)-\d{3,4})\s(.+)(#time \d{1,9}['s', 'h', 'm', 'd'])$/
+            ),
+        },
+    },
+    rules: {
+        'subject-case': [0, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+        'type-empty': () => {
+            console.log(
+                ChalkLog.bgRed.bold('Wrong pattern commit:'),
+                ChalkLog.bgCyanBright.bold('<type>') + ChalkLog.bgGreenBright.bold('[(<scope>)]:'),
+                ChalkLog.bgCyanBright.bold('<issue key> <main message>'),
+                ChalkLog.bgGreenBright.bold(
+                    '[#time <duration> [<work log description>]] Ex: 3h, 3h 30m, 1d'
+                )
+            );
+            // console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+            return [2, 'never'];
+        },
+        'subject-empty': [2, 'never'],
+    },
+};
+
+export default Configuration;
